0fe5cda709c90847320f5c2e1c650962c3c0c32f
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-05-18 23:02:04 -0500

    main layout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -3,10 +3,14 @@ import { Outlet } from "react-router-dom"
 const AuthLayout = () => {
   return (
   <>
-    <div>AuthLayout</div>
+    <main className="container mx-auto mt-5 md:mt-20 p-5 md:flex md:justify-center">
+      <div className="md:w-2/3 lg:1/2 bg-red-100">
+
+        <Outlet/>
+      </div>
+    </main>
 
     {/* es para inyectar lo que esta en el outlet */}
-    <Outlet/>
   </>
   )
 }
