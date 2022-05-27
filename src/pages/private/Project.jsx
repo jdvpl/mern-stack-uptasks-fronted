8aa6c16b1d40b5f907e9a054bb76e12c4c290340
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-05-27 18:08:52 -0500

    set modal headless UI

--- src/pages/private/Project.jsx
+++ src/pages/private/Project.jsx
@@ -1,11 +1,13 @@
 import {useParams} from 'react-router-dom'
 import useProjects from '../../hooks/useProjects';
-import {useEffect} from 'react';
+import {useEffect,useState} from 'react';
 
 import {Link} from 'react-router-dom';
 import Mensaje from '../../components/Mensaje';
+import ModalTaskForm from '../../components/private/ModalTaskForm';
 
 const Project = () => {
+  const [modal, setmodal] = useState(false)
   const {getProject,project,loadingProject,deleteProject,alert}=useProjects();
   
   const {id:idRouter}=useParams();
@@ -61,7 +63,17 @@ const Project = () => {
           <button className="uppercase font-bold" onClick={deleteProjectHandle}>Delete</button>
       </div>
       </div>
+
     </div>
+      <button 
+        onClick={()=>setmodal(true)}
+      type="button" className="uppercase font-bold text-sm px-5 py-3 w-full md:w-auto rounded-lg bg-sky-400 text-center text-white mt-5 flex gap-2 items-center justify-center" >
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
+      </svg>
+        Add Task
+        </button>
+        <ModalTaskForm modal={modal} setmodal={setmodal}/>
     </>
     )
     )
