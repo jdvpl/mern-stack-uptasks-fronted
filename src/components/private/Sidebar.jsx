b6aec85aba74ccf2a7c927de7772a32c76bbc2cf
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-06-19 21:07:43 -0500

    getting email of user

--- src/components/private/Sidebar.jsx
+++ src/components/private/Sidebar.jsx
@@ -8,6 +8,8 @@ const Sidebar = () => {
   return (
     <aside className="md:w-80 lg:w-96 px-5 py-10">
       <p className="text-xl font-bold">Hi!, {auth.name}</p>
+      <p className="text-sm uppercase">Email: {auth.email}</p>
+
       <Link to="create-project" className="bg-sky-600  w-full text-white uppercase block font-bld mt-5 text-center rounded-lg p-3">New Project</Link>
     </aside>
   )
